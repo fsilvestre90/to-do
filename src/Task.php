6f371455864ac29f907bc5e45014057f2c84b934
Filipe Silvestre & Deron Johnson <student@epicodus.com>
2015-08-24 11:18:27 -0700

    Add ALL THE THINGSgit rm --cached composer.lockgit rm --cached composer.lock Just join table functionality

---
+++ src/Task.php
@@ -0,0 +1,129 @@
+<?php
+class Task
+{
+    private $description;
+    private $id;
+    private $due_date;
+
+    function __construct($description, $id = null, $due_date = null)
+    {
+        $this->description = $description;
+        $this->id = $id;
+        $this->due_date = $due_date;
+    }
+
+    function setDescription($new_description)
+    {
+        $this->description = (string) $new_description;
+    }
+
+    function getDescription()
+    {
+        return $this->description;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    function setDueDate($new_due_date)
+    {
+        $this->description = (string) $new_due_date;
+    }
+
+    function getDueDate()
+    {
+        return $this->due_date;
+    }
+
+    function save()
+    {
+        $statement = $GLOBALS['DB']->exec("INSERT INTO tasks (description)
+            VALUES ('{$this->getDescription()}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    static function getAll()
+    {
+        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+        $tasks = array();
+        foreach($returned_tasks as $task){
+            $description = $task['description'];
+            $id = $task['id'];
+            $new_task = new Task($description, $id);
+            array_push($tasks, $new_task);
+        }
+        return $tasks;
+    }
+
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM tasks;");
+    }
+
+    // static function find($search_name)
+    // {
+    //     $found_tasks = array();
+    //     $tasks = Task::getAll();
+    //     foreach($tasks as $task){
+    //         if ($task->getDescription() == $search_name){
+    //             array_push($found_tasks, $task->getDescription());
+    //         }
+    //     }
+    //     return $found_tasks;
+    // }
+
+    static function find($search_id)
+    {
+        $found_task = null;
+        $tasks = Task::getAll();
+        foreach($tasks as $task) {
+            $task_id = $task->getId();
+            if ($task_id == $search_id) {
+                $found_task = $task;
+            }
+        }
+        return $found_task;
+    }
+
+
+
+    function update($new_description)
+    {
+        $GLOBALS['DB']->exec("UPDATE tasks SET description = '{$new_description}' WHERE id = {$this->getId()};");
+        $this->setDescription($new_description);
+    }
+
+    function delete()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM tasks WHERE id = {$this->getId()};");
+        $GLOBALS['DB']->exec("DELETE FROM categories_tasks WHERE task_id = {$this->getId()};");
+    }
+
+    function addCategory($category)
+    {
+        $GLOBALS['DB']->exec("INSERT INTO categories_tasks (category_id, task_id) VALUES ({$category->getId()}, {$this->getId()});");
+    }
+
+    function getCategories()
+    {
+        $query = $GLOBALS['DB']->query("SELECT category_id FROM categories_tasks WHERE task_id = {$this->getId()};");
+        $category_ids = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $categories = array();
+        foreach ($category_ids as $id) {
+            $category_id = $id['category_id'];
+            $result = $GLOBALS['DB']->query("SELECT * FROM categories WHERE id = {$category_id};");
+            $returned_category = $result->fetchAll(PDO::FETCH_ASSOC);
+
+            $name = $returned_category[0]['name'];
+            $id = $returned_category[0]['id'];
+            $new_category = new Category($name, $id);
+            array_push($categories, $new_category);
+        }
+        return $categories;
+    }
+}
+
+?>
---
+++ tests/CategoryTest.php
@@ -0,0 +1,185 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Category.php";
+    require_once "src/Task.php";
+
+    $server = 'mysql:host=localhost;dbname=to_do_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CategoryTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Category::deleteAll();
+            Task::deleteAll();
+        }
+
+        function test_getName()
+        {
+            //Arrange
+            $name = "Work Stuff";
+            $test_Category = new Category($name);
+
+            //Act
+            $result = $test_Category->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $id = 1;
+            $test_Category = new Category($name, $id);
+
+            //Act
+            $result = $test_Category->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function testSave()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $id = 1;
+            $test_category = new Category($name, $id);
+            $test_category->save();
+
+            //Act
+            $result = Category::getAll();
+
+            //Assert
+            $this->assertEquals($test_category, $result[0]);
+        }
+
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $name2 = "Home stuff";
+            $test_Category = new Category($name);
+            $test_Category->save();
+            $test_Category2 = new Category($name2);
+            $test_Category2->save();
+
+            //Act
+            $result = Category::getAll();
+
+            //Assert
+            $this->assertEquals([$test_Category, $test_Category2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $name2 = "Home stuff";
+            $test_Category = new Category($name);
+            $test_Category->save();
+            $test_Category2 = new Category($name2);
+            $test_Category2->save();
+
+            //Act
+            Category::deleteAll();
+            $result = Category::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
+        function test_find()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $name2 = "Home stuff";
+            $test_Category = new Category($name);
+            $test_Category->save();
+            $test_Category2 = new Category($name2);
+            $test_Category2->save();
+
+            //Act
+            $result = Category::find($test_Category->getId());
+
+            //Assert
+            $this->assertEquals($test_Category, $result);
+        }
+        function testAddTask()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $id = 1;
+            $test_category = new Category($name, $id);
+            $test_category->save();
+
+            $description = "File reports";
+            $id2 = 2;
+            $test_task = new Task($description, $id2);
+            $test_task->save();
+
+            //Act
+            $test_category->addTask($test_task);
+
+            //Assert
+            $this->assertEquals($test_category->getTasks(), [$test_task]);
+        }
+
+        function testGetTasks()
+        {
+            //Arrange
+            $name = "Home stuff";
+            $id = 1;
+            $test_category = new Category($name, $id);
+            $test_category->save();
+
+            $description = "Wash the dog";
+            $id2 = 2;
+            $test_task = new Task($description, $id2);
+            $test_task->save();
+
+            $description2 = "Take out the trash";
+            $id3 = 3;
+            $test_task2 = new Task($description2, $id3);
+            $test_task2->save();
+
+            //Act
+            $test_category->addTask($test_task);
+            $test_category->addTask($test_task2);
+
+            //Assert
+            $this->assertEquals($test_category->getTasks(), [$test_task, $test_task2]);
+        }
+
+        function testDelete()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $id = 1;
+            $test_category = new Category($name, $id);
+            $test_category->save();
+
+            $description = "File reports";
+            $id2 = 2;
+            $test_task = new Task($description, $id2);
+            $test_task->save();
+
+            //Act
+            $test_category->addTask($test_task);
+            $test_category->delete();
+
+            //Assert
+            $this->assertEquals([], $test_task->getCategories());
+        }
+    }
+?>
